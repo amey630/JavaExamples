b98400442fc0f145021d836ccd64b58b708ba603
HP <HP@DESKTOP-NOE6USJ>
2021-07-18 18:12:41 +0530

    functional interface example

---
+++ src/com/amey/java8/FunctionaInterfaceExample.java
@@ -0,0 +1,29 @@
+package com.amey.java8;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+
+public class FunctionaInterfaceExample {
+	
+	public static void main(String[] args) {
+		
+		BiConsumer<Integer, Integer> bi = (a,b) -> System.out.println(a+b);
+		bi.accept(5, 7);
+		
+		BiFunction<Integer, Integer, String> bif = (a,b) -> Integer.toString(a+b);
+		System.out.println(bif.apply(5, 8));
+		
+		BinaryOperator<String> bio = (a,b)-> a+b;
+		System.out.println(bio.apply("Hello", " world"));
+		
+		UnaryOperator<String> uo = a-> a+ " my world";
+		System.out.println(uo.apply("Hello"));
+		
+		Predicate<String> p = s->s.contains("H");
+		System.out.println(p.test("Helga"));
+	}
+
+}
