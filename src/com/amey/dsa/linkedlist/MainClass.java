3bce944186a3ed52e8e4d6799c0e00e0ba50722b
Amey <amey630@gmail.com>
2022-12-09 00:04:37 +0530

    linked list done

--- src/com/amey/dsa/linkedlist/MainClass.java
+++ src/com/amey/dsa/linkedlist/MainClass.java
@@ -3,8 +3,20 @@ package com.amey.dsa.linkedlist;
 public class MainClass {
 
     public static void main(String[] args) {
-        LinkedList linkedList = new LinkedList(4);
-        linkedList.printList();
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+
+        System.out.println("LL before reverse():");
+        myLinkedList.printList();
+
+        myLinkedList.reverse();
+
+        System.out.println("\nLL after reverse():");
+        myLinkedList.printList();
+
+
     }
 
 
