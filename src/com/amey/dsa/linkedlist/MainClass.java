e8cb665eb88e070989341c3b8db6bbbb01d807f4
Amey <amey630@gmail.com>
2022-12-08 14:27:29 +0530

    linked list first commit

---
+++ src/com/amey/dsa/linkedlist/MainClass.java
@@ -0,0 +1,12 @@
+package com.amey.dsa.linkedlist;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(4);
+        linkedList.printList();
+    }
+
+
+
+}
